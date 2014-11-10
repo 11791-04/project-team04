191adc4c491c293168167fad745cd26bf45197b0
nikwolfe7 <nikwolfe7@gmail.com>
2014-11-09 21:30:35 -0500

    ConceptSearchResults added to jcas

--- src/main/java/ConceptAnalysisEngine.java
+++ src/main/java/ConceptAnalysisEngine.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -6,31 +9,45 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 
+import util.GoPubMedServiceFactory;
+import util.GoPubMedServiceProxy;
 import util.TypeFactory;
+import edu.cmu.lti.oaqa.bio.bioasq.services.GoPubMedService;
+import edu.cmu.lti.oaqa.bio.bioasq.services.OntologyServiceResponse;
+import edu.cmu.lti.oaqa.bio.bioasq.services.OntologyServiceResponse.Finding;
 import edu.cmu.lti.oaqa.type.kb.Concept;
+import edu.cmu.lti.oaqa.type.kb.ConceptMention;
+import edu.cmu.lti.oaqa.type.retrieval.ConceptSearchResult;
 
 
 public class ConceptAnalysisEngine extends JCasAnnotator_ImplBase {
 
+  private GoPubMedServiceProxy service;
+  private List<ConceptSearchResult> conceptSearchResults;
+  
   @Override
   public void initialize(UimaContext aContext) throws ResourceInitializationException {
     super.initialize(aContext);
+    this.service = new GoPubMedServiceProxy();
+    this.conceptSearchResults = new LinkedList<ConceptSearchResult>();
   }
   
   @Override
   public void process(JCas jcas) throws AnalysisEngineProcessException {
-    System.out.print(jcas.getAnnotationIndex());
-    FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
-    if (iter.isValid()) {
-      iter.moveToNext();
-      Concept c = TypeFactory.createConcept(jcas, "www.something.com");
-      createConceptAnnotation(jcas, c);
+    String query = jcas.getSofaDataString();
+    List<Finding> findings = service.getFindingsFromQuery(query);
+    for(Finding f : findings) {
+      createConceptSearchResultFromFinding(jcas, f);
     }
   }
 
-  private void createConceptAnnotation(JCas jcas, Concept c) {
-    // TODO Auto-generated method stub
-    
+  private void createConceptSearchResultFromFinding(JCas jcas, Finding f) {
+    OntologyServiceResponse.Concept c = f.getConcept();
+    Concept concept = TypeFactory.createConcept(jcas, c.getLabel(), c.getUri());
+    ConceptSearchResult csr = TypeFactory.createConceptSearchResult(jcas, concept, c.getUri());
+    System.out.println("Concept: " + concept.getName() + " URI: " + concept.getUris().getNthElement(0));
+    conceptSearchResults.add(csr);
+    csr.addToIndexes(jcas);
   }
 
 }
