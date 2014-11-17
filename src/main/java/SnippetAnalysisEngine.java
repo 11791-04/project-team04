c6c575eab6a40b60eaef428da929ff8fde8b7056
josephcc <josephcc@cmu.edu>
2014-11-16 23:11:58 -0500

    Snippet AE: Sentence segmentation, tokenization

--- src/main/java/SnippetAnalysisEngine.java
+++ src/main/java/SnippetAnalysisEngine.java
@@ -1,9 +1,22 @@
+import java.util.List;
+
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+
+import edu.cmu.lti.oaqa.type.retrieval.Document;
+
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.FSIterator;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
+import snippetextraction.SentenceInfo;
+import util.MetalWebService;
+import util.TextUtils;
+import util.TypeConstants;
 
 public class SnippetAnalysisEngine extends JCasAnnotator_ImplBase {
 
@@ -12,14 +25,46 @@ public class SnippetAnalysisEngine extends JCasAnnotator_ImplBase {
     // TODO Auto-generated method stub
     super.initialize(aContext);
   }
-  
-  
+
   @Override
   public void process(JCas aJCas) throws AnalysisEngineProcessException {
-    // TODO Auto-generated method stub
-    
+    try {
+      FSIterator<?> it;
+      it = aJCas.getFSIndexRepository().getAllIndexedFS(
+              aJCas.getRequiredType("edu.cmu.lti.oaqa.type.retrieval.Document"));
+      while (it.hasNext()) {
+        Document doc = (Document) it.next();
+        if (doc.getSearchId() != null
+                && doc.getSearchId().equals(TypeConstants.SEARCH_ID_GOLD_STANDARD)) {
+          continue;
+        }
+        try {
+	        String pmid = doc.getDocId();
+	        JSONObject docFull = MetalWebService.getDocFullTextJSon(pmid);
+			    JSONArray sectionArr = docFull.getJSONArray("sections");
+			    System.out.println(sectionArr.length());
+			    
+			    int secidx = 0;
+			    for(int i=0; i<sectionArr.length(); i++) {
+			      String section = (String) sectionArr.get(i);
+			      List<SentenceInfo> sentences = TextUtils.stanfordSentenceTokenizer(section);
+			      for(SentenceInfo sentence : sentences) {
+			        System.out.println(sentence);
+			        List<String> tokens = TextUtils.stanfordTokenizer(sentence.content);
+			      }
+			      secidx++;
+		      }
+        } catch (Exception e) {
+		      // TODO Auto-generated catch block
+        }
+      }
+    } catch (CASException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
   }
-  
+
   @Override
   public void collectionProcessComplete() throws AnalysisEngineProcessException {
     // TODO Auto-generated method stub
--- src/main/java/snippetextraction/SentenceInfo.java
+++ src/main/java/snippetextraction/SentenceInfo.java
@@ -4,10 +4,10 @@ import docretrieval.DocInfo;
 
 public class SentenceInfo {
 
-  String content;
-  int startIndex;
-  int endIndex;
-  DocInfo hostDoc;
+  public String content;
+  public int startIndex;
+  public int endIndex;
+  public DocInfo hostDoc;
   
   public SentenceInfo(String content, int startIndex, int endIndex, DocInfo hostDoc) {
     super();
@@ -15,6 +15,12 @@ public class SentenceInfo {
     this.startIndex = startIndex;
     this.endIndex = endIndex;
     this.hostDoc = hostDoc;
+  }
+  
+  public String toString() {
+    String out = "";
+    out += "<SENT " + startIndex + ":" + endIndex + " doc:" + hostDoc + " " + content + ">";
+    return out;
   }
 }
 
