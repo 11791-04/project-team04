700bd7d00d1a9b669dc532f4b634e1f17ae20b55
josephcc <josephcc@cmu.edu>
2014-11-20 16:57:49 -0500

    Score the sentences using cosine similarity in snippet AE

--- src/main/java/SnippetAnalysisEngine.java
+++ src/main/java/SnippetAnalysisEngine.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
@@ -10,6 +11,7 @@ import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import docretrieval.CollectionStatistics;
 import docretrieval.DocInfo;
 import docretrieval.stemmer.KrovetzStemmer;
+import edu.cmu.lti.oaqa.type.input.Question;
 import edu.cmu.lti.oaqa.type.retrieval.Document;
 
 import org.apache.uima.cas.CASException;
@@ -19,6 +21,10 @@ import org.apache.uima.resource.ResourceInitializationException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import scoring.CandidateAnswer;
+import scoring.CandidateAnswerAdapter;
+import scoring.Similarity;
+import scoring.SimilarityFactory;
 import snippetextraction.SentenceInfo;
 import util.MetalWebService;
 import util.TextUtils;
@@ -27,7 +33,7 @@ import util.TypeConstants;
 public class SnippetAnalysisEngine extends JCasAnnotator_ImplBase {
 
   KrovetzStemmer stemmer;
-  
+
   @Override
   public void initialize(UimaContext aContext) throws ResourceInitializationException {
     // TODO Auto-generated method stub
@@ -37,6 +43,14 @@ public class SnippetAnalysisEngine extends JCasAnnotator_ImplBase {
 
   @Override
   public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    FSIterator<?> qit = aJCas.getAnnotationIndex(Question.type).iterator();
+    Question question = null;
+    if (qit.hasNext()) {
+      question = (Question) qit.next();
+      System.out.println(question.getText());
+      System.out.println(question.getId());
+      System.out.println(question.getQuestionType());
+    }
     CollectionStatistics cStat = new CollectionStatistics();
     ArrayList<SentenceInfo> allSentences = new ArrayList<SentenceInfo>();
     try {
@@ -50,31 +64,31 @@ public class SnippetAnalysisEngine extends JCasAnnotator_ImplBase {
           continue;
         }
         try {
-	        String pmid = doc.getDocId();
-	        JSONObject docFull = MetalWebService.getDocFullTextJSon(pmid);
-			    JSONArray sectionArr = docFull.getJSONArray("sections");
-			    System.out.println(sectionArr.length());
-			    
-	        Map<String, String> fieldTextMap = new HashMap<String, String>();
-	        fieldTextMap.put("title", doc.getTitle());
-			    for(int i=0; i<sectionArr.length(); i++) {
-		        fieldTextMap.put("section:" + i, (String) sectionArr.get(i));
-			    }
-	        DocInfo docInfo = new DocInfo(pmid, fieldTextMap, null, stemmer);
-	        cStat.addDoc(docInfo);
-			    
-			    for(int i=0; i<sectionArr.length(); i++) {
-			      String section = (String) sectionArr.get(i);
-			      List<SentenceInfo> sentences = TextUtils.stanfordSentenceTokenizer(section);
-			      for(SentenceInfo sentence : sentences) {
-			        sentence.hostDoc = docInfo;
-			        sentence.sectionIndex = i;
-			        allSentences.add(sentence);
-			        System.out.println(sentence);
-			      }
-		      }
+          String pmid = doc.getDocId();
+          JSONObject docFull = MetalWebService.getDocFullTextJSon(pmid);
+          JSONArray sectionArr = docFull.getJSONArray("sections");
+          System.out.println(sectionArr.length());
+
+          Map<String, String> fieldTextMap = new HashMap<String, String>();
+          fieldTextMap.put("title", doc.getTitle());
+          for (int i = 0; i < sectionArr.length(); i++) {
+            fieldTextMap.put("section:" + i, (String) sectionArr.get(i));
+          }
+          DocInfo docInfo = new DocInfo(pmid, fieldTextMap, null, stemmer);
+          cStat.addDoc(docInfo);
+
+          for (int i = 0; i < sectionArr.length(); i++) {
+            String section = (String) sectionArr.get(i);
+            List<SentenceInfo> sentences = TextUtils.stanfordSentenceTokenizer(section);
+            for (SentenceInfo sentence : sentences) {
+              sentence.hostDoc = docInfo;
+              sentence.sectionIndex = i;
+              allSentences.add(sentence);
+              System.out.println(sentence);
+            }
+          }
         } catch (Exception e) {
-		      // TODO Auto-generated catch block
+          // TODO Auto-generated catch block
         }
       }
       cStat.finalize();
@@ -82,7 +96,21 @@ public class SnippetAnalysisEngine extends JCasAnnotator_ImplBase {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
+    SentenceInfo questionInfo = new SentenceInfo(question.getText(), -1, -1, -1, null);
+    scoring.Question query = new scoring.QuestionAdapter(questionInfo);
+    Similarity similarity = SimilarityFactory.getNewSimilarity(SimilarityFactory.cos);
 
+    for (SentenceInfo sentence : allSentences) {
+      CandidateAnswer answer = new CandidateAnswerAdapter(sentence);
+      sentence.score = similarity.computeSimilarity(query, answer);
+    }
+    
+    allSentences = (ArrayList<SentenceInfo>) allSentences.stream()
+            .filter(e -> e.score >= 0.0001)
+            .sorted((e1, e2) -> Double.compare(e2.score, e1.score))
+            .collect(Collectors.toList())
+            ;
+    
   }
 
   @Override
