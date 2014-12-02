69c58e30179b3f42822c1e68bbfd3a707967b228
nikwolfe7 <nikwolfe7@gmail.com>
2014-12-01 22:45:43 -0500

    Fixed Jaccard Similarity bullshit

--- src/main/java/snippets/scoring/strategy/JaccardSimilarityStrategy.java
+++ src/main/java/snippets/scoring/strategy/JaccardSimilarityStrategy.java
@@ -36,8 +36,8 @@ public class JaccardSimilarityStrategy implements Similarity {
     Double count1, count2, minCount = 0.0, maxCount = 0.0;
     Set<String> set = Sets.union(qv.keySet(), dv.keySet());
     for (String key : set) {
-      count1 = qv.containsKey(key) ? qv.get(key) : 1.0;
-      count2 = dv.containsKey(key) ? dv.get(key) : 1.0;
+      count1 = qv.containsKey(key) ? qv.get(key).doubleValue() : 1.0;
+      count2 = dv.containsKey(key) ? dv.get(key).doubleValue() : 1.0;
       minCount += (count1 < count2 ? count1 : count2);
       maxCount += (count1 > count2 ? count1 : count2);
     }
