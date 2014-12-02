58d965b1f4319d1c241b5293d03bde4870e80fee
josephcc <josephcc@cmu.edu>
2014-12-01 21:38:11 -0500

    Add javadoc to QuestionReader

--- src/main/java/descriptorimpl/QuestionReader.java
+++ src/main/java/descriptorimpl/QuestionReader.java
@@ -1,4 +1,5 @@
 package descriptorimpl;
+
 import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
@@ -21,46 +22,47 @@ import json.gson.TestQuestion;
 import json.gson.TestSet;
 
 /**
- * CollectionReader for reading questions.
+ * CollectionReader for reading questions and gold standard answers from the input file.
+ * 
  * @author josephc1
  *
  */
 public class QuestionReader extends CollectionReader_ImplBase {
-  
+
   private List<TestQuestion> inputs;
+
   private int numberOfQuestions;
-  
+
   @Override
+  /**
+   * The initializer loads the single input file to memory and performs some syntax clean up.
+   */
   public void initialize() throws ResourceInitializationException {
     super.initialize();
     String filePath = "/BioASQ-SampleData1B.json";
-    
+
     inputs = Lists.newArrayList();
 
     Object value = filePath;
     if (String.class.isAssignableFrom(value.getClass())) {
-      inputs = TestSet
-          .load(getClass().getResourceAsStream(
-              String.class.cast(value))).stream()
-          .collect(toList());
+      inputs = TestSet.load(getClass().getResourceAsStream(String.class.cast(value))).stream()
+              .collect(toList());
     } else if (String[].class.isAssignableFrom(value.getClass())) {
-      inputs = Arrays
-          .stream(String[].class.cast(value))
-          .flatMap(
-              path -> TestSet.load(
-                  getClass().getResourceAsStream(path))
-                  .stream()).collect(toList());
+      inputs = Arrays.stream(String[].class.cast(value))
+              .flatMap(path -> TestSet.load(getClass().getResourceAsStream(path)).stream())
+              .collect(toList());
     }
     // trim question texts
-    inputs.stream()
-        .filter(input -> input.getBody() != null)
-        .forEach(
-            input -> input.setBody(input.getBody().trim()
-                .replaceAll("\\s+", " ")));
-    
+    inputs.stream().filter(input -> input.getBody() != null)
+            .forEach(input -> input.setBody(input.getBody().trim().replaceAll("\\s+", " ")));
+
     numberOfQuestions = inputs.size();
   }
 
+  /**
+   * Create one CAS for each questions in the input file. All gold standard answers, if existed, are
+   * also added to the index.
+   */
   @Override
   public void getNext(CAS aCAS) throws IOException, CollectionException {
     JCas jcas;
@@ -69,7 +71,7 @@ public class QuestionReader extends CollectionReader_ImplBase {
     } catch (CASException e) {
       throw new CollectionException(e);
     }
-    
+
     TestQuestion question = inputs.remove(0);
     jcas.setDocumentText(question.getBody());
     JsonCollectionReaderHelper.addQuestionToIndex(question, jcas);
@@ -80,9 +82,14 @@ public class QuestionReader extends CollectionReader_ImplBase {
     return inputs.size() > 0;
   }
 
+  /**
+   * The unit for progress is number of questions processed out of the total number of questions in
+   * the input file.
+   */
   @Override
   public Progress[] getProgress() {
-    return new Progress[] { new ProgressImpl(numberOfQuestions - inputs.size(), numberOfQuestions, Progress.ENTITIES) };
+    return new Progress[] { new ProgressImpl(numberOfQuestions - inputs.size(), numberOfQuestions,
+            Progress.ENTITIES) };
   }
 
   @Override
