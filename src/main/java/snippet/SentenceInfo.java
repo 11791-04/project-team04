ba733f9603e83d583173987568c993173b9f69f4
josephcc <josephcc@cmu.edu>
2014-12-01 21:42:36 -0500

    Add javadoc to sentenceinfo

--- src/main/java/snippet/SentenceInfo.java
+++ src/main/java/snippet/SentenceInfo.java
@@ -2,15 +2,46 @@ package snippet;
 
 import document.DocInfo;
 
+/**
+ * The SentenceInfo class represent one sentence in the full text of a document retrieved from
+ * pubmed.
+ * 
+ * @author josephcc
+ *
+ */
 public class SentenceInfo {
 
+  /**
+   * The textual content of the sentence.
+   */
   public String content;
+
+  /**
+   * the start index in the full text
+   */
   public int startIndex;
+
+  /**
+   * the end index in the full text
+   */
   public int endIndex;
+
+  /**
+   * the corresponding document object
+   */
   public DocInfo hostDoc;
+
+  /**
+   * the section index in the full text. we do not support sentences (snippets) that cross multiple
+   * sections.
+   */
   public String sectionIndex;
+
+  /**
+   * Similarity score comparing to the question
+   */
   public Double score;
-  
+
   public String getContent() {
     return content;
   }
@@ -26,8 +57,9 @@ public class SentenceInfo {
   public DocInfo getHostDoc() {
     return hostDoc;
   }
-  
-  public SentenceInfo(String content, String sectionIndex, int startIndex, int endIndex, DocInfo hostDoc, Double score) {
+
+  public SentenceInfo(String content, String sectionIndex, int startIndex, int endIndex,
+          DocInfo hostDoc, Double score) {
     super();
     this.content = content;
     this.sectionIndex = sectionIndex;
@@ -37,7 +69,8 @@ public class SentenceInfo {
     this.score = score;
   }
 
-  public SentenceInfo(String content, String sectionIndex, int startIndex, int endIndex, DocInfo hostDoc) {
+  public SentenceInfo(String content, String sectionIndex, int startIndex, int endIndex,
+          DocInfo hostDoc) {
     super();
     this.content = content;
     this.sectionIndex = sectionIndex;
@@ -45,12 +78,14 @@ public class SentenceInfo {
     this.endIndex = endIndex;
     this.hostDoc = hostDoc;
   }
-  
-  
+
+  /**
+   * pretty print function
+   */
   public String toString() {
     String out = "";
-    out += "<SENT " + startIndex + ":" + endIndex + " doc:" + hostDoc.uri + " -" + content.replace('\n', ' ') + " " + String.format("%.3f", score) + ">";
+    out += "<SENT " + startIndex + ":" + endIndex + " doc:" + hostDoc.uri + " -"
+            + content.replace('\n', ' ') + " " + String.format("%.3f", score) + ">";
     return out;
   }
 }
-
